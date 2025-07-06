4f2c485dcf9f761ab57635570a8e976639b788fb
Erika Palladino <eripalladino@gmail.com>
2025-07-06 12:13:32 -0300

    blog y formulario de contacto

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const nav = document.getElementById("nav");
 iconoNav.addEventListener("click", () => {
   nav.classList.toggle("visible");
 });
+
+const posts = document.querySelectorAll(".post");
+
+function checkPosts() {
+  const triggerBottom = window.innerHeight * 0.85; // ajustar el punto de disparo
+
+  posts.forEach((post) => {
+    const postTop = post.getBoundingClientRect().top;
+
+    if (postTop < triggerBottom) {
+      post.classList.add("visible");
+    } else {
+      post.classList.remove("visible");
+    }
+  });
+}
+
+window.addEventListener("scroll", checkPosts);
+checkPosts(); // para ejecutar también al cargar la página
